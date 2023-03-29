103c2444c0a597bf744ff7a43b5753649bd89cee
Joaquin Bazan <Joako14_16@hotmail.com.com>
2023-03-28 23:26:54 -0300

    Pre-entrega3 Bazan Joaquin

--- Js/script.js
+++ Js/script.js
@@ -1,46 +1,62 @@
+const btnCalcular = document.getElementById('btnCalcular'),
+inputMonto = document.querySelector("input[type='number']"),
+opcionCuotas = document.getElementById('cantCuotas'),
+txtMonto = document.getElementById('txtModificable');
+
 function cuota (a, b) {
     let resultado = a / b
     return resultado
 }
 
-function cliente (nombre,telefono) {
-    this.nombre = nombre;
-    this.telefono = telefono;
-}
-
-const listaClientes = [ {nombre:'Agustin Antello',telefono:111222333},{nombre:'Leonardo Cabrera',telefono:444555666} ];
-
-let monto = parseInt(prompt('Ingrese el importe que desea solicitar. El monto mínimo es de $10000'));
-if(monto<10000){
-    alert('Monto no permitido');
-} else {
-    let cuotas = parseInt(prompt('Ingrese la cantidad de cuotas deseadas. Mínimo cuotas 6 y máximo 18'));
-    while ((cuotas>18)||(cuotas<6)) {
-        alert('Número de cuotas no permitidas');
-        cuotas = parseInt(prompt('Ingrese otra cantidad de cuotas'))
-    }
+btnCalcular.onclick=(evento)=>{
     
-    let resultadoCuota = cuota(monto,cuotas);
-    alert('El monto de su cuota sin interes será de $' + resultadoCuota);
+    const cuotas = cuota(inputMonto.value,opcionCuotas.value);
+    evento.preventDefault();
+    txtMonto.innerText = '$' + cuotas.toFixed(2);
+
+    console.log(inputMonto.value);
+    console.log(opcionCuotas.value);
+    console.log(txtMonto);
+}
+
+// function cliente (nombre,telefono) {
+//     this.nombre = nombre;
+//     this.telefono = telefono;
+// }
+
+// const listaClientes = [ {nombre:'Agustin Antello',telefono:111222333},{nombre:'Leonardo Cabrera',telefono:444555666} ];
+
+// let monto = parseInt(prompt('Ingrese el importe que desea solicitar. El monto mínimo es de $10000'));
+// if(monto<10000){
+//     alert('Monto no permitido');
+// } else {
+//     let cuotas = parseInt(prompt('Ingrese la cantidad de cuotas deseadas. Mínimo cuotas 6 y máximo 18'));
+//     while ((cuotas>18)||(cuotas<6)) {
+//         alert('Número de cuotas no permitidas');
+//         cuotas = parseInt(prompt('Ingrese otra cantidad de cuotas'))
+//     }
     
-}
-let interesado = parseInt(prompt('¿Desea solicitar el prestamo?, digíte 1 para si o 2 para no'));
-if (interesado===1) {
+//     let resultadoCuota = cuota(monto,cuotas);
+//     alert('El monto de su cuota sin interes será de $' + resultadoCuota);
+    
+// }
+// let interesado = parseInt(prompt('¿Desea solicitar el prestamo?, digíte 1 para si o 2 para no'));
+// if (interesado===1) {
 
-let nombre = prompt('Ingrese su nombre');
-let telefono = prompt('Ingrese su número de teléfono');
+// let nombre = prompt('Ingrese su nombre');
+// let telefono = prompt('Ingrese su número de teléfono');
 
-let cliente1 = {nombre,telefono};
+// let cliente1 = {nombre,telefono};
 
-listaClientes.push(cliente1);
+// listaClientes.push(cliente1);
 
-alert('Muchas gracias, nos estaremos comunicando conusted a la brevedad');
+// alert('Muchas gracias, nos estaremos comunicando conusted a la brevedad');
 
-} else {
-    alert('Gracias por interactuar con nosotros')
-}
+// } else {
+//     alert('Gracias por interactuar con nosotros')
+// }
 
-console.log(listaClientes);
+// console.log(listaClientes);
 
 
 
